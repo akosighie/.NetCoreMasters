95d74fde97847ebdf45972a2c61e6f9047f7bbe0
DESKTOP-FGGA7BD\GNR <ghie>
2021-06-19 19:05:16 +0800

    for session4  map copntrollers

--- DotNetCoreMasters/DotNetCoreMasters/Controllers/ItemsController.cs
+++ DotNetCoreMasters/DotNetCoreMasters/Controllers/ItemsController.cs
@@ -11,29 +11,62 @@ using System.Threading.Tasks;
 
 namespace DotNetCoreMasters.Controllers
 {
-    public class ItemsController : Controller
+    [Route("[controller]")]
+    public class ItemsController : ControllerBase
     {
-        public int Get(int userId)
+        [HttpGet]
+        public IActionResult GetAll()
         {
-            var res = new ItemService();
-            var listOfStrings = res.GetAll(userId);
-
-            return 0;
-        }
-
-        public void Post(ItemCreateBindingModel itemModel)
-        {
-            var res = new ItemService();
-            var itemDTOmap = new ItemDTO();
-
-            var configMapper = new MapperConfiguration(cfg =>
+            var listItems = new List<string>()
             {
-                cfg.CreateMap<ItemCreateBindingModel, ItemDTO>();
-            });
-            IMapper mapper = configMapper.CreateMapper();
-            var mappedItemDTO = mapper.Map(itemModel, itemDTOmap);
+                "Item1",
+                "Item2",
+                "Item3"
+            };
 
-            res.Save(mappedItemDTO);
+            return Ok(listItems);
         }
+
+        [HttpGet("/items/{itemId}")]
+        public IActionResult Get(int itemId)
+        {
+
+            return Ok(itemId);
+        }
+
+
+        [HttpGet("/items/filterBy")]
+        public IActionResult GetByFilters([FromQuery] Dictionary<string, string> filters)
+        {
+            var filter = filters;
+
+            return Ok(filter);
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] ItemCreateBindingModel itemCreateModel)
+        {
+            var model = itemCreateModel;
+
+            return Ok();
+        }
+
+        [HttpPut("/items/{itemId}")]
+        public IActionResult Put(int itemId, [FromBody] ItemCreateBindingModel itemCreateModel)
+        {
+            var newId = itemId;
+            var model = itemCreateModel;
+
+            return Ok();
+        }
+
+        [HttpDelete("/items/{itemId}")]
+        public IActionResult Put(int itemId)
+        {
+            var newId = itemId;
+
+            return Ok();
+        }
+
     }
 }
