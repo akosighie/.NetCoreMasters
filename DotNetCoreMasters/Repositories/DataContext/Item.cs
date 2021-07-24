25fac1ffc9c5625995872fda1f5528a73113e668
DESKTOP-FGGA7BD\GNR <ghie>
2021-07-24 18:24:35 +0800

    fixed property names and added key annotations

--- DotNetCoreMasters/Repositories/DataContext/Item.cs
+++ DotNetCoreMasters/Repositories/DataContext/Item.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace Repositories.DataContext
 {
     public class Item
     {
+        [Key]
         public string ItemName { get; set; }
         public int ItemId { get; set; }
     }
--- DotNetCoreMasters/Repositories/DataContext/User.cs
+++ DotNetCoreMasters/Repositories/DataContext/User.cs
@@ -1,14 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace Repositories.DataContext
 {
     public class User
     {
-        public string name { get; set; }
-        public string email { get; set; }
-        public string userName { get; set; }
-        public string password { get; set; }
+        [Key]
+        public int  UserId { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
     }
 }
