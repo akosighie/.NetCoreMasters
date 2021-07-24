8fc1340c985dd703c873216a1d908a4068fe5145
DESKTOP-FGGA7BD\GNR <ghie>
2021-07-24 16:13:30 +0800

    added EF package added DBContext

---
+++ DotNetCoreMasters/Repositories/DataContext/DataContextModel/DatabaseContext.cs
@@ -0,0 +1,24 @@
+﻿using Repositories.DataContext;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Text;
+
+namespace Repositories.DatabaseContext.DataContextModel
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext() : base("DatabaseConnection")
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Item> Items { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
---
+++ DotNetCoreMasters/Repositories/DataContext/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repositories.DataContext
+{
+    public class User
+    {
+        public string name { get; set; }
+        public string email { get; set; }
+        public string userName { get; set; }
+        public string password { get; set; }
+    }
+}
